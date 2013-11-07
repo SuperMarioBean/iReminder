b46956fa8adbfc93ae46f0fbd4339e53abb71fc2
David Fu <david.fu.zju@gmail.com>
2013-11-08 04:15:50 +0800

    Initial Commit

---
+++ iReminder/FBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FBAppDelegate.h
+//  iReminder
+//
+//  Created by 傅 斌 on 11/8/13.
+//  Copyright (c) 2013 傅 斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iReminder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iReminder
+//
+//  Created by 傅 斌 on 11/8/13.
+//  Copyright (c) 2013 傅 斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FBAppDelegate class]));
+    }
+}
